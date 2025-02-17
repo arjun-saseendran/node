0b53f733855307da919bb554dad8aab92f02d76c
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-17 09:27:27 +0530

    add calculatorModule.js

---
+++ calculatorModule.js
@@ -0,0 +1,11 @@
+const add = (num1, num2) => console.log(num1 + num2);
+const sub = (num1, num2) => console.log(num1 - num2);
+const mul = (num1, num2) => console.log(num1 * num2);
+const div = (num1, num2) => console.log(num1 / num2);
+
+module.exports = {
+  addition: add,
+  subtraction: sub,
+  multiplication: mul,
+  division: div,
+};
--- global.js
+++ global.js
@@ -1,1 +1,8 @@
-console.log(global);
+const calculatorModule = require("./calculatorModule");
+
+// console.log(global);
+
+calculatorModule.addition(5, 3);
+calculatorModule.subtraction(5, 1);
+calculatorModule.multiplication(3, 2);
+calculatorModule.division(39, 29);
