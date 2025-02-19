8d3b8f49cf8b0b68d854902f6180144dca1502be
arjun-saseendran <arjunsaseendran@icloud.com>
2025-02-19 14:29:46 +0530

    add os.js

---
+++ os.js
@@ -0,0 +1,8 @@
+const os = require("os");
+
+console.log(os.arch()); // Display architecture
+console.log(os.platform()); // Display platform
+console.log(os.networkInterfaces()); // Display network interfaces
+console.log(os.cpus()); // Display cpu details
+console.log(os.totalmem()); // Display total memory
+console.log(os.freemem()); // Show free memory
